16a1f024fd6a01449431d17ab1bbfa10b40a0b38
Jin Pan <jinxp1@gmail.com>
2014-05-01 22:14:10 -0400

    + basic simulation

--- main.js
+++ main.js
@@ -7,17 +7,55 @@
 Physics(function (world) {
     var renderer = Physics.renderer('canvas', {
         el: 'viewport',
-        width: 500,
-        height: 500
+        width: 1000,
+        height: 1000
     });
     world.add(renderer);
 
-    var square = Physics.body('rectangle', {
+    var target = Physics.body('circle', {
+        x: 550,
+        y: 550,
+        treatment: 'static',
+        radius: 45
+    });
+
+    var targetAttraction = Physics.behavior('attractor', {
+         pos: target.state.pos,
+    });
+
+    var collisionDetector = Physics.behavior('body-collision-detection', {
+        check: true
+    });
+
+    var capsule = Physics.body('circle', {
         x: 250,
         y: 250,
-        width: 50,
-        height: 50
+        radius: 20,
+        mass: 2
     });
-    world.add(square);
+
+    capsule.applyForce({
+        x: 0.005,
+        y: 0.002
+    });
+
+    world.add(target);
+    world.addBehavior(targetAttraction);
+    world.addBehavior(collisionDetector);
+    world.add(capsule);
+
+
+    world.on('step', function() {
+        world.render();
+    });
+    Physics.util.ticker.on(function(time) {
+        world.step(time);
+    });
+
+    world.on('collisions:detected', function() {
+        alert("collision");
+    });
+
     world.render();
 });
+// Physics.util.ticker.start();
