bb64fcee8182433d33cd4b64bb024a112e81ab9c
Jen Liu <jkliu@mit.edu>
2014-05-06 15:59:52 -0400

    rearranged planets in level 2

--- js/2.js
+++ js/2.js
@@ -1,6 +1,6 @@
 var target = Physics.body('circle', {
-    x: 750,
-    y: 750,
+    x: 800,
+    y: 600,
     treatment: 'kinematic',
     radius: 15
 });
@@ -13,8 +13,8 @@ target.view = new Image();
 target.view.src = ('css/images/mars.jpg');
 
 var capsule = Physics.body('circle', {
-    x: 250,
-    y: 250,
+    x: 200,
+    y: 200,
     radius: 10,
     mass: 2
 });
@@ -23,13 +23,13 @@ capsule.view = new Image();
 capsule.view.src = ('css/images/spaceship.png');
 
 // mercury
-addPlanet(200, 200, 20, "mercury.jpg");
+addPlanet(150, 150, 20, "mercury.jpg");
 
 // venus
-addPlanet(400, 400, 15, "venus.jpg");
+addPlanet(400, 350, 15, "venus.jpg");
 
 // earth
-addPlanet(600, 600, 15, "earth.jpg");
+addPlanet(600, 500, 15, "earth.jpg");
 
 world.add(target);
 world.addBehavior(targetAttraction);
