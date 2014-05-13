38fd895a955464760596f1a9e5b0bdddaeb85e9c
Ariana Eisenstein <ajeisens@mit.edu>
2014-05-13 16:32:35 -0400

    3a, 3b added. black hole exists. transitions untested

---
+++ js/31.js
@@ -0,0 +1,41 @@
+var target = Physics.body('circle', {
+    x: 800,
+    y: 600,
+    treatment: 'kinematic',
+    radius: 15
+});
+
+var targetAttraction = Physics.behavior('attractor', {
+    pos: target.state.pos
+});
+
+var blackHoleAttraction = Physics.behavior('attractor', {
+    strength: 10,
+    pos: {x: 500,y: 500}
+});
+
+//target.view = new Image();
+//target.view.src = ('css/images/mars.jpg');
+
+var capsule = Physics.body('circle', {
+    x: 260,
+    y: 240,
+    radius: 10,
+    mass: 2
+});
+
+capsule.view = new Image();
+capsule.view.src = ('css/images/spaceship.png');
+
+world.add(capsule);
+
+
+level = 31;
+
+world.add(target);
+world.addBehavior(targetAttraction);
+world.addBehavior(blackHoleAttraction);
+
+world.render();
+
+go = true;
---
+++ js/32.js
@@ -0,0 +1,45 @@
+var target = Physics.body('circle', {
+    x: 800,
+    y: 600,
+    treatment: 'kinematic',
+    radius: 15
+});
+
+var targetAttraction = Physics.behavior('attractor', {
+    pos: target.state.pos
+});
+
+var blackHoleAttraction = Physics.behavior('attractor', {
+    strength: 10,
+    pos: {x: 500,y: 500}
+});
+
+//target.view = new Image();
+//target.view.src = ('css/images/mars.jpg');
+
+var capsule = Physics.body('circle', {
+    x: 260,
+    y: 240,
+    radius: 10,
+    mass: 2
+});
+
+capsule.view = new Image();
+capsule.view.src = ('css/images/spaceship.png');
+
+world.add(capsule);
+
+// earth
+addPlanet(50, 50, 20, "earth.jpg");
+
+// venus
+addPlanet(50, 100, 20, "venus.jpg");
+
+
+level = 32;
+
+world.add(target);
+world.addBehavior(targetAttraction);
+
+world.render();
+
